a99578046647fb20f49e114a3eae4730454724d1
chenyilong <luohanchenyilong@163.com>
2015-12-24 03:09:14 +0800

    avoid you to return NO for `-enableScrollWhenPlaceHolderViewShowing`

--- CYLTableViewPlaceHolder/CYLTableViewPlaceHolderDelegate.h
+++ CYLTableViewPlaceHolder/CYLTableViewPlaceHolderDelegate.h
@@ -17,8 +17,9 @@
 
 @optional
 /*!
- @brief  enable tableView scroll or not when place holder view is showing,it is disabled by default.
- @return enable tableView scroll or not
+ @brief enable tableView scroll when place holder view is showing,it is disabled by default.
+ @attention There is no need to return  NO , it will be NO by default
+ @return enable tableView scroll, you can only return YES
  */
 - (BOOL)enableScrollWhenPlaceHolderViewShowing;
 
