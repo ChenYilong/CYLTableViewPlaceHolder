3e37c04867db5675ecb85d1cb7d58931873933ba
chenyilong <luohanchenyilong@163.com>
2015-12-24 03:29:55 +0800

    improved formatting

--- CYLTableViewPlaceHolder/CYLTableViewPlaceHolderDelegate.h
+++ CYLTableViewPlaceHolder/CYLTableViewPlaceHolderDelegate.h
@@ -17,8 +17,8 @@
 
 @optional
 /*!
- @brief enable tableView scroll when place holder view is showing,it is disabled by default.
- @attention There is no need to return  NO , it will be NO by default
+ @brief enable tableView scroll when place holder view is showing, it is disabled by default.
+ @attention There is no need to return  NO, it will be NO by default
  @return enable tableView scroll, you can only return YES
  */
 - (BOOL)enableScrollWhenPlaceHolderViewShowing;
