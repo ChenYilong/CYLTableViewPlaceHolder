26fbaca32105b2711fd3ab65855c3b837cb00ec7
chenyilong <luohanchenyilong@163.com>
2015-12-28 11:21:10 +0800

    change owner info

--- CYLTableViewPlaceHolder/UITableView+CYLTableViewPlaceHolder.h
+++ CYLTableViewPlaceHolder/UITableView+CYLTableViewPlaceHolder.h
@@ -2,7 +2,7 @@
 //  UITableViewController+CYLTableViewPlaceHolder.h
 //  CYLNoResultTableViewController
 //
-//  Created by 微博@iOS程序犭袁 (http://weibo.com/luohanchenyilong/) on 15/12/23.
+//  Created by 微博@iOS程序犭袁 ( http://weibo.com/luohanchenyilong/ ) on 15/12/23.
 //  Copyright © 2015年 https://github.com/ChenYilong . All rights reserved.
 //
 
--- Example/AppDelegate.h
+++ Example/AppDelegate.h
@@ -2,7 +2,7 @@
 //  AppDelegate.h
 //  CYLNoResultTableViewController
 //
-//  Created by 微博@iOS程序犭袁 (http://weibo.com/luohanchenyilong/) on 15/12/22.
+//  Created by 微博@iOS程序犭袁 ( http://weibo.com/luohanchenyilong/ ) on 15/12/22.
 //  Copyright © 2015年 https://github.com/ChenYilong . All rights reserved.
 //
 
